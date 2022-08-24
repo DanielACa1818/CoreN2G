31b1a69b4407c78f8a75cd039d35098308099586
DanielACa1818 <99919474+DanielACa1818@users.noreply.github.com>
2022-08-23 18:00:04 -0600

    Changes made to support STM32F446

--- src/STM32/ccmram.cpp
+++ src/STM32/ccmram.cpp
@@ -1,5 +1,5 @@
 #include "CoreImp.h"
-#if !STM32H7
+#if 0
 extern char _sccmram;					// defined in linker script
 extern char _eccmram;					// defined in linker script
 extern char _ccmramend;					// defined in linker script
