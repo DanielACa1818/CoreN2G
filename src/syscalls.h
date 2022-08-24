51a4fe9c42237779bc6bb7a9d48d5ba05b984776
DanielACa1818 <99919474+DanielACa1818@users.noreply.github.com>
2022-08-23 18:36:28 -0600

    Changed syscalls.h if directive to stm32f446

--- src/syscalls.h
+++ src/syscalls.h
@@ -68,7 +68,7 @@ void *CoreAllocPermanent(size_t sz, std::align_val_t align) noexcept
 		OutOfMemoryHandler();
 	}
 	return ret;
-#elif STM32F4 && 0
+#elif STM32F4 && !STM32F446
 	return CoreAllocCCMRAMPermanent(sz, align);
 #else
 	char * const newHeapLimit = reinterpret_cast<char *>(reinterpret_cast<uint32_t>(heapLimit - sz) & ~((uint32_t)align - 1));
